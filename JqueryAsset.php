3fb95b74c04d9df63803afdc39987d69db2b4013
xiewulong <xiewulong@vip.qq.com>
2015-02-07 23:24:07 +0800

    xui

---
+++ JqueryAsset.php
@@ -0,0 +1,25 @@
+<?php
+/*!
+ * xui - jquery Asset
+ * xiewulong <xiewulong@vip.qq.com>
+ * https://github.com/xiewulong/yii2-xui
+ * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
+ * create: 2015/2/7
+ * update: 2015/2/7
+ * version: 0.0.1
+ */
+
+namespace yii\xui;
+
+use Yii;
+use yii\web\AssetBundle;
+
+class JqueryAsset extends AssetBundle{
+
+	public $sourcePath = '@bower/jquery/dist';
+
+	public $js = [
+		'jquery.min.js',
+	];
+
+}
---
+++ JqueryXAsset.php
@@ -0,0 +1,29 @@
+<?php
+/*!
+ * xui - jqueryX Asset
+ * xiewulong <xiewulong@vip.qq.com>
+ * https://github.com/xiewulong/yii2-xui
+ * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
+ * create: 2015/2/7
+ * update: 2015/2/7
+ * version: 0.0.1
+ */
+
+namespace yii\xui;
+
+use Yii;
+use yii\web\AssetBundle;
+
+class JqueryXAsset extends AssetBundle{
+
+	public $sourcePath = '@xiewulong/jqueryX';
+
+	public $js = [
+		'jquery.x.min.js',
+	];
+
+	public $depends = [
+		'yii\xui\JqueryAsset',
+	];
+
+}
