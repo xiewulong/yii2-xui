884c98945e8869d0e9f378d2f086613e8434058d
xiewulong <xiewulong@vip.qq.com>
2015-02-09 00:11:01 +0800

    xui

--- JqueryXAsset.php
+++ JqueryXAsset.php
@@ -5,7 +5,7 @@
  * https://github.com/xiewulong/yii2-xui
  * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
  * create: 2015/2/7
- * update: 2015/2/7
+ * update: 2015/2/9
  * version: 0.0.1
  */
 
--- WwwAsset.php
+++ WwwAsset.php
@@ -4,8 +4,8 @@
  * xiewulong <xiewulong@vip.qq.com>
  * https://github.com/xiewulong/yii2-xui
  * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
- * create: 2015/2/7
- * update: 2015/2/7
+ * create: 2015/2/9
+ * update: 2015/2/9
  * version: 0.0.1
  */
 
@@ -14,12 +14,16 @@ namespace yii\xui;
 use Yii;
 use yii\web\AssetBundle;
 
-class JqueryAsset extends AssetBundle{
+class WwwAsset extends AssetBundle{
 
 	public $sourcePath = '@xiewulong/www/static/public';
 
+	public $css = [
+		'css/public.css',
+	];
+
 	public $js = [
-		'jquery.min.js',
+		'js/jquery.min.js',
 	];
 
 }
