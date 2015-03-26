29ad119239b23c924107c3665c984c0ab82a1905
xiewulong <xiewulong@vip.qq.com>
2015-03-26 11:35:27 +0800

    xui

--- UeditorAction.php
+++ UeditorAction.php
@@ -9,22 +9,67 @@ use yii\imagine\Image;
 
 class UeditorAction extends Action{
 
+	private $config;
+
 	private $types = [
 		'image' => ['image/gif', 'image/jpeg', 'image/pjpeg'],
 	];
 
+	public function init(){
+		parent::init();
+
+		$this->config = Json::decode(preg_replace('/(\/\*[\s\S]+?\*\/)|(\s+)/', '', file_get_contents(__DIR__ . '/static/plugins/ueditor/php/config.json')));
+		$this->config['imageAllowFiles'] = ['.gif', '.jpg', '.jpeg'];
+	}
+
 	public function run($action){
 		switch($action){
 			case 'config':
-				return preg_replace('/(\/\*[\s\S]+?\*\/)|(\s+)/', '', file_get_contents(__DIR__ . '/static/plugins/ueditor/php/config.json'));
+				return Json::encode($this->config);
 				break;
 			case 'uploadimage':
-			case 'uploadscrawl':
-			case 'uploadvideo':
-			case 'uploadfile':
-				return true;
+				return $this->fileupload();
 				break;
 		}
+	}
+
+	private function fileupload(){
+		$request = \Yii::$app->request;
+		$name = 'upfile';
+		$min = 0;
+		$max = $this->config['imageMaxSize'];
+		$type = 'image';
+		$sizes = null;
+		$oss = 'images';
+		$response = ['state' => '没有文件被上传'];
+
+		if(!empty($name) && !empty($_FILES)){
+			$_file = $_FILES[$name];
+			if(!empty($min) && $_file['size'] < $min){
+				$response['state'] = \Yii::t('common', 'File size too small');
+			}else if(!empty($max) && $_file['size'] > $max){
+				$response['state'] = \Yii::t('common', 'File size too large');
+			}else if(!empty($type) && !in_array($_file['type'], $this->types[$type])){
+				$response['state'] = \Yii::t('common', 'Please upload the right file type');
+			}else{
+				$manager = \Yii::createObject(Yii::$app->components['fileupload']);
+				$ossActive = isset($manager->oss[$oss]);
+				$file = $manager->createFile(array_pop(explode('.', $_file['name'])));
+				if(move_uploaded_file($_file['tmp_name'], $file['tmp'])){
+					$url = $manager->finalFile($file, $oss);
+					$response['state'] = 'SUCCESS';
+					$response['title'] = $_file['name'];
+					$response['type'] = $_file['type'];
+					$response['size'] = $_file['size'];
+					$response['url'] = $response['original'] = $url;
+					if($ossActive){
+						unlink($file['tmp']);
+					}
+				}
+			}
+		}
+
+		return Json::encode($response);
 	}
 	
 }
