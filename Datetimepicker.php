6fcb9b75c2cf6c65991ca0d394b6e55d21cf3ba2
xiewulong <xiewulong@vip.qq.com>
2015-06-17 10:43:44 +0800

    xui

--- Datetimepicker.php
+++ Datetimepicker.php
@@ -5,7 +5,7 @@
  * https://github.com/xiewulong/yii2-xui
  * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
  * create: 2015/1/23
- * update: 2015/6/11
+ * update: 2015/6/17
  * version: 0.0.1
  */
 
