8bffdfb6a2ba51406b034b13fbc3a871b1e6154e
xiewulong <xiewulong@vip.qq.com>
2016-08-18 02:39:01 +0800

    xui

--- ModuleAsset.php
+++ ModuleAsset.php
@@ -5,7 +5,7 @@
  * https://github.com/xiewulong/yii2-xui
  * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
  * create: 2016/8/2
- * update: 2016/8/8
+ * update: 2016/8/16
  * since: 0.0.2
  */
 
@@ -23,14 +23,11 @@ class ModuleAsset extends AssetBundle {
 	public function init() {
 		parent::init();
 
-		$namespace = $this->namespace ? : \Yii::$app->controller->module->id;
-		$filename = $namespace . '.' . \Yii::$app->controller->id;
-		$this->css[] = 'css/' . $namespace . '.css';
-		$this->css[] = 'css/' . $filename . '.css';
-		$this->js[] = 'js/' . $namespace . '.js';
-		$this->js[] = 'js/' . $filename . '.js';
-
 		$this->sourcePath = \Yii::$app->controller->module->basePath . DIRECTORY_SEPARATOR . $this->distPath;
+
+		$namespace = $this->namespace ? : \Yii::$app->controller->module->id;
+		$this->css[] = 'css/' . $namespace . '.css';
+		$this->js[] = 'js/' . $namespace . '.js';
 	}
 
 }
---
+++ ModuleControllerAsset.php
@@ -0,0 +1,27 @@
+<?php
+/*!
+ * yii - asset - module.controller
+ * xiewulong <xiewulong@vip.qq.com>
+ * https://github.com/xiewulong/yii2-xui
+ * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
+ * create: 2016/8/16
+ * update: 2016/8/16
+ * since: 0.0.2
+ */
+
+namespace yii\xui;
+
+use Yii;
+
+class ModuleControllerAsset extends ModuleAsset {
+
+	public function init() {
+		parent::init();
+
+		$namespace = $this->namespace ? : \Yii::$app->controller->module->id;
+		$filename = $namespace . '.' . \Yii::$app->controller->id;
+		$this->css[] = 'css/' . $filename . '.css';
+		$this->js[] = 'js/' . $filename . '.js';
+	}
+
+}
