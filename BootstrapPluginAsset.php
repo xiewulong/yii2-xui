2393dd4670abfd46f1402eda9480d761f49d9fa0
xiewulong <xiewulong@vip.qq.com>
2017-01-06 01:25:40 +0800

    xui

--- BootstrapPluginAsset.php
+++ BootstrapPluginAsset.php
@@ -5,7 +5,7 @@
  * https://github.com/xiewulong/yii2-xui
  * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
  * create: 2017/1/5
- * update: 2017/1/5
+ * update: 2017/1/6
  * since: 0.0.1
  */
 
@@ -21,6 +21,7 @@ class BootstrapPluginAsset extends AssetBundle {
 	public $depends = [
 		'yii\xui\BootstrapCssAsset',
 		'yii\xui\JqueryAsset',
+		'yii\xui\TetherAsset',
 	];
 
 	public function init() {
--- TetherAsset.php
+++ TetherAsset.php
@@ -1,11 +1,11 @@
 <?php
 /*!
- * yii - asset - requirejs
+ * yii - asset - tether
  * xiewulong <xiewulong@vip.qq.com>
  * https://github.com/xiewulong/yii2-xui
  * https://raw.githubusercontent.com/xiewulong/yii2-xui/master/LICENSE
- * create: 2015/2/9
- * update: 2016/9/5
+ * create: 2017/1/6
+ * update: 2017/1/6
  * since: 0.0.1
  */
 
@@ -14,14 +14,14 @@ namespace yii\xui;
 use Yii;
 use yii\components\AssetBundle;
 
-class RequirejsAsset extends AssetBundle {
+class TetherAsset extends AssetBundle {
 
-	public $sourcePath = '@yii/xui/dist';
+	public $sourcePath = '@npm/tether/dist';
 
 	public function init() {
 		parent::init();
 
-		$this->js[] = 'js/require' . $this->minimal . '.js';
+		$this->js[] = 'js/tether' . $this->minimal . '.js';
 	}
 
 }
